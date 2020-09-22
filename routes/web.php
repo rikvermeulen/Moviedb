330640c1dd507ca6c26b6e277dad4df1f73518ae
Rik Vermeulen <rik.vermeulen.1997@live.nl>
2020-09-22 16:19:57 +0200

    project update to laravel ^8.0, Fixed route file to new format

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,22 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MoviesController;
 
+Route::get('/', [MoviesController::class, 'index'])->name('movies.index');
+Route::get('/movies/{movie}', [MoviesController::class, 'show'])->name('movies.show');
 
-route::get('/', 'MoviesController@index')->name('movies.index');
-route::get('/movies/{movie}', 'MoviesController@show')->name('movies.show');
+/*route::get('/', 'MoviesController@index')->name('index');*/
+/*route::get('/movies/{movie}', 'MoviesController@show')->name('movies.show');*/
 
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+/*Route::get('/home', 'HomeController@index')->name('home');*/
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+
+
+
