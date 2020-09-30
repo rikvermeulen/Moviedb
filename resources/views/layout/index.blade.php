9a8f3a1078934f84fd6a89e71b31cba833e2c75c
rikvermeulen <rik.vermeulen.1997@live.nl>
2020-09-30 15:23:55 +0200

    #Added -None
    #bugfixes
    -faster loading time
    -cleaned up blade files with less statements
    -Fixed unnecessary loops and moved all te code to view models to make it a bit cleaner and faster. (pkg:laravel-view-models)

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -9,7 +9,7 @@
                 <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">Popular movies</h2>
                 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
                     @foreach($popularMovies as $movie)
-                        <x-movie-card :movie="$movie" :genres="$genres"/>
+                        <x-movie-card :movie="$movie"/>
 
                     @endforeach
                 </div>
@@ -19,7 +19,7 @@
                 <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">Now Playing</h2>
                 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
                     @foreach($nowPlayingMovies as $movie)
-                        <x-movie-card :movie="$movie" :genres="$genres"/>
+                        <x-movie-card :movie="$movie"/>
 
                     @endforeach
                 </div>
--- resources/views/layout/show.blade.php
+++ resources/views/layout/show.blade.php
@@ -6,18 +6,16 @@
         @include ('layout.partials.header')
         <div class="movie-info border-b border-gray-800">
             <div class="container mx-auto px-4 py-16 flex flex-col md:flex-row">
-                <img src="{{ 'https://image.tmdb.org/t/p/w500/'.$movie['poster_path'] }}" alt="poster" class="w-64 lg:w-96">
+                <img src="{{ $movie['poster_path'] }}" alt="parasite" class="w-64 lg:w-96">
                 <div class="md:ml-24">
                     <h2 class="text-4xl mt-4 md:mt-0 font-semibold">{{ $movie['title'] }}</h2>
                     <div class="flex flex-wrap items-center text-gray-400 text-sm">
                         <span><svg class="fill-current text-orange-500 w-4" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"></path><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path><path d="M0 0h24v24H0z" fill="none"></path></svg></span>
-                        <span class="ml-1">{{ $movie['vote_average'] * 10 .'%' }}</span>
+                        <span class="ml-1">{{ $movie['vote_average'] }}</span>
                         <span class="mx-2">|</span>
-                        <span>{{ \Carbon\Carbon::parse($movie['release_date'])->format('M d, Y') }}</span>
+                        <span>{{ $movie['release_date'] }}</span>
                         <span class="mx-2">|</span>
-                        <span>@foreach($movie['genres'] as $genre)
-                                {{ $genre['name']}}@if(!$loop->last), @endif
-                            @endforeach</span>
+                        <span>{{ $movie['genres'] }}</span>
                     </div>
                     <p class="text-gray-300 mt-8">
                         {{ $movie['overview'] }}
@@ -25,13 +23,11 @@
                     <div class="mt-12">
                         <h4 class="text-white font-semibold">Featured Cast</h4>
                         <div class="flex mt-4">
-                            @foreach( $movie['credits']['crew'] as $crew)
-                                @if ($loop->index < 2)
+                            @foreach ($movie['crew'] as $crew)
                                 <div class="mr-8">
                                     <div>{{ $crew['name'] }}</div>
                                     <div class="text-sm text-gray-400">{{ $crew['job'] }}</div>
                                 </div>
-                                @endif
                             @endforeach
                         </div>
                     </div>
@@ -78,20 +74,18 @@
             <div class="container mx-auto px-4 py-16 ">
                 <div class="text-4xl font-semibold">Cast</div>
                 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
-                    @foreach( $movie['credits']['cast'] as $cast)
-                       @if ($loop->index < 5)
-                       <div class="mt-8">
-                            <a href="">
-                                <img src="{{ 'https://image.tmdb.org/t/p/w300/'.$cast['profile_path'] }}" alt="" class="hover:opacity-75 transition ease-in-out duration-150">
+                    @foreach ($movie['cast'] as $cast)
+                        <div class="mt-8">
+                            <a href="#">
+                                <img src="{{ 'https://image.tmdb.org/t/p/w300/'.$cast['profile_path'] }}" alt="actor1" class="hover:opacity-75 transition ease-in-out duration-150">
                             </a>
                             <div class="mt-2">
-                                <a href="" class="text-lig mt-2 hover:text-gray:300">{{ $cast['name'] }}</a>
+                                <a href="#" class="text-lg mt-2 hover:text-gray:300">{{ $cast['name'] }}</a>
                                 <div class="text-sm text-gray-400">
                                     {{ $cast['character'] }}
                                 </div>
                             </div>
-                       </div>
-                        @endif
+                        </div>
                     @endforeach
                 </div>
             </div>
@@ -101,7 +95,7 @@
             <div class="container mx-auto px-4 py-16">
                 <h2 class="text-4xl font-semibold">Images</h2>
                 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-                    @foreach ($movie['images']['backdrops'] as $image)
+                    @foreach ($movie['images'] as $image)
                         <div class="mt-8">
                             <a
                                 @click.prevent="
